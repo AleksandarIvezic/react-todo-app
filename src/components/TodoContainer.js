596697709d26e26d8b709234ef86df7699c0c17c
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-20 12:43:39 +0200

    Create and implement checkbox listener

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,29 +3,39 @@ import TodosList from './TodosList';
 import Header from "./Header";
 class TodoContainer extends React.Component {
   state = {
- todos: [
-   {
-     id: 1,
-     title: "Setup development environment",
-     completed: true
-   },
-   {
-     id: 2,
-     title: "Develop website and add content",
-     completed: false
-   },
-   {
-     id: 3,
-     title: "Deploy to live server",
-     completed: false
-   }
- ]
-};
+  todos: [
+    {
+      id: 1,
+      title: "Setup development environment",
+      completed: true
+    },
+    {
+      id: 2,
+      title: "Develop website and add content",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Deploy to live server",
+      completed: false
+    }
+  ]
+  };
+  handleChange = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
   render() {
     return (
       <React.Fragment>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
       </React.Fragment>
     )
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,12 @@ import React from "react"
 const TodoItem = (props) => {
   return (
     <li>
-    <input type='checkbox' /> {props.todo.title}
+    <input 
+    type='checkbox' 
+    checked={props.todo.completed} 
+    onChange={() => props.handleChangeProps(props.todo.id)}
+    />
+    {props.todo.title}
     </li>
   )
 }
