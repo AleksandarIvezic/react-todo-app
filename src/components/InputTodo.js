02431136d7afeda046aa676210a7886369db4d37
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-20 14:46:23 +0200

    Add verification condition for input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -11,10 +11,14 @@ class InputTodo extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: ''
-    });
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: ''
+      });
+    } else {
+      alert('Please write item')
+    }
   };
   render() {
     return (
