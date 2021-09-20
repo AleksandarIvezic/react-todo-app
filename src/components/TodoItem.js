d12fd13a5c98de0aee6d201323543b1a020edea3
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-20 11:31:57 +0200

    Create TodoList and call it in TodosList

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+  render() {
+    return (
+      <li>
+        {this.props.todo.title}
+      </li>
+    )
+  }
+}
+
+export default TodoItem;
