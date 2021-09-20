43e851718b3e96a78344b405f7836e2f6b3fcf04
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-20 20:33:43 +0200

    Deploy app on github pages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./functionBased/components/TodoContainer"
 import './functionBased/App.css'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <TodoContainer />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'))
