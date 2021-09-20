980559f986cbf0a2c31d0d038fa5cfcbe9db0981
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-20 18:56:56 +0200

    Install and implement react router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-//component file
 import TodoContainer from "./functionBased/components/TodoContainer"
-
-//stylesheet
 import './functionBased/App.css'
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'))
