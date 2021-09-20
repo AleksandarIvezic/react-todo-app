d51a0831fa5fccc5a64dd0c6c9fd59ab1d29f092
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-20 11:36:42 +0200

    Convert TodoItem class into function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,7 @@
-import React from "react";
+import React from "react"
 
-class TodoItem extends React.Component {
-  render() {
-    return (
-      <li>
-        {this.props.todo.title}
-      </li>
-    )
-  }
+const TodoItem = (props) => {
+  return <li>{props.todo.title}</li>
 }
 
-export default TodoItem;
+export default TodoItem
